e564077bcce0638cdf0b081f73b00d571b885dde
OlaruAlexis <104926664+OlaruAlexis@users.noreply.github.com>
2022-05-04 20:30:46 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String firstRomanNumber = scanner.next();
+        String sign = scanner.next();
+        String secondRomanNumber = scanner.next();
+        if (scanner.hasNext()) {
+            throw new Exception("Wrong input");
+        }
+        scanner.close();
+        List<String> romana = new ArrayList<>(Arrays.asList("I","II", "III", "IV", "V", "VI", "VII", "VII", "IX", "X"));
+        List<String> arabic = new ArrayList<>(Arrays.asList("1","2", "3", "4", "5", "6", "7", "8", "9", "10"));
+        int rezult = 0;
+        boolean isRoman=true;
+        if (romana.contains(firstRomanNumber) && romana.contains(secondRomanNumber)) {
+            isRoman = true;
+        } else if(arabic.contains(firstRomanNumber) && arabic.contains(secondRomanNumber)) {
+            isRoman = false;
+        } else {
+            throw new Exception("Wrong input");
+        }
+        int firstNumber;
+        int secondNumber;
+        String result = "";
+        if (isRoman) {
+            firstNumber = RomanNumbers.romanToArabic(firstRomanNumber);
+            secondNumber = RomanNumbers.romanToArabic(secondRomanNumber);
+        } else {
+            firstNumber = Integer.parseInt(firstRomanNumber);
+            secondNumber = Integer.parseInt(secondRomanNumber);
+        }
+            switch (sign) {
+                case ("+"):
+                    rezult = firstNumber + secondNumber;
+                    break;
+                case ("-"):
+                    rezult = firstNumber - secondNumber;
+                    break;
+                case ("*"):
+                    rezult = firstNumber * secondNumber;
+                    break;
+                case ("/"):
+                    rezult = firstNumber / secondNumber;
+                    break;
+                default:
+                    throw new Exception("Wrong sign");
+            }
+        if (isRoman) {
+            result = RomanNumbers.arabicToRoman(rezult);
+        } else {
+            result += rezult;
+        }
+        System.out.println(result);
+
+        }
+    }
+
